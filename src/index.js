4387884320beef45c51a6c3d91daeebbd339f354
jreyes8631 <jreyes8631@gmail.com>
2021-09-14 20:56:32 -0400

    refactor the geolocation code to stay dry

--- src/index.js
+++ src/index.js
@@ -18,12 +18,8 @@ class App extends React.Component {
 
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        this.setState({lat: position.coords.latitude});
-      },
-      err => {
-        this.setState({errorMessage: err.message});
-      }
+      position => this.setState({lat: position.coords.latitude}),
+      err => this.setState({errorMessage: err.message})
    );
   }
 
