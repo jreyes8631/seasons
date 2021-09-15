dc34f5e48128ffceee49abb55c508e747178b463
jreyes8631 <jreyes8631@gmail.com>
2021-09-14 21:42:36 -0400

    add getSeason function to show the seasin fir the user

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,6 +1,16 @@
 import React from 'react'
 
-const SeasonDisplay = () => {
+const getSeasons = (lat, month) => {
+   if(month > 2 && month < 9) {
+       return lat > 0 ? 'Summer' : 'Winter';
+   } else {
+       return lat > 0 ? 'Winter' : 'Summer';
+   }
+}
+
+const SeasonDisplay = props => {
+    const season = getSeasons(props.lat, new Date().getMonth());
+    console.log(season)
    
     return <div>Season Display</div>;
 }
